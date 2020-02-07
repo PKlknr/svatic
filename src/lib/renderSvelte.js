5cdb68ba92cbc6bd606e6beba25d7ebfc12988cc
pk <PKlknr@users.noreply.github.com>
2020-02-07 10:25:09 +0100

    WIP add back rollup for preprocessing
    can be switched in renderSvelte

--- src/lib/renderSvelte.js
+++ src/lib/renderSvelte.js
@@ -1,6 +1,11 @@
 const {injectIntoHead, maybeLog} = require('.');
 const relative = require('require-relative');
 const path = require('path');
+const {rollup} = require('rollup');
+const rollupSveltePlugin = require('rollup-plugin-svelte');
+// for renderSvelteRollup
+// eslint-disable-next-line no-unused-vars
+const internal = require('svelte/internal');
 
 require('svelte/register')({
   dev: true,
@@ -9,23 +14,47 @@ require('svelte/register')({
 // require look inside node_modules when path does not start with / or ./
 const requireablePath = p => (!path.isAbsolute(p) ? './' : '') + p;
 
-const renderSvelte = (srcDir, filename, props) => {
+const renderSvelteRequire = (srcDir, filename, props) => {
   const mod = relative.resolve(
     requireablePath(path.join(srcDir, filename)),
     process.cwd(),
   );
   /* eslint-disable-next-line global-require */
-  const Comp = relative(mod).default;
-
-  return Comp.render(props);
+  return relative(mod).default.render(props);
 };
 
-const renderSvelteWithStyle = (srcDir, filename, props) => {
-  maybeLog('makeHtmlWithStyle', srcDir, filename, props);
-  const rendered = renderSvelte(srcDir, filename, props);
-  return injectIntoHead(`\n<style>${rendered.css.code}</style>\n`)(
-    rendered.html,
+
+const renderSvelteRollup = (srcDir, filename, props) =>
+  rollup({
+    input: (path.join(srcDir, filename)),
+    plugins: [
+      rollupSveltePlugin({
+        generate: 'ssr',
+        hydratable: true,
+      }),
+    ],
+    external: ['svelte/internal'],
+  }).then(bundle =>
+    bundle.generate({
+      format: 'cjs',
+    }),
+  ).then(gen =>
+    // eslint-disable-next-line no-eval
+    eval(gen.output[0].code).render(props),
   );
+
+
+const renderSvelte = async (srcDir, filename, props) => {
+  const t = Date.now();
+  const r = await renderSvelteRequire(srcDir, filename, props);
+  console.log(filename, Date.now() - t);
+  return r;
 };
+
+const renderSvelteWithStyle = (srcDir, filename, props) =>
+  maybeLog('makeHtmlWithStyle', srcDir, filename, props) ||
+  renderSvelte(srcDir, filename, props).then(rendered =>
+    injectIntoHead(`\n<style>${rendered.css.code}</style>\n`)(rendered.html),
+  );
 
 module.exports.renderSvelteWithStyle = renderSvelteWithStyle;
--- src/render.js
+++ src/render.js
@@ -4,12 +4,14 @@ const {maybeLog} = require('./lib');
 const {renderSvelteWithStyle} = require('./lib/renderSvelte');
 const {injectHydratorLoader} = require('./hydrator');
 
-
 const renderPage = (srcDir, destDir, src, dest, hydratable, props) => {
-  const html = renderSvelteWithStyle(srcDir, src);
-  const h2 = hydratable ? injectHydratorLoader(src, props)(html) : html;
-  maybeLog('renderPage writes', path.join(destDir, dest));
-  return fs.promises.writeFile(path.join(destDir, dest), h2);
+  renderSvelteWithStyle(srcDir, src)
+    .then(html => (hydratable ? injectHydratorLoader(src, props)(html) : html))
+    .then(
+      html =>
+        maybeLog('renderPage writes', path.join(destDir, dest)) ||
+        fs.promises.writeFile(path.join(destDir, dest), html),
+    );
 };
 
 module.exports.renderPage = renderPage;
